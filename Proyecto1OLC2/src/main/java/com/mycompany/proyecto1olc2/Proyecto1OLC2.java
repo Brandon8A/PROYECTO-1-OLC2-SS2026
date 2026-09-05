ac78ebb4d6921fc47d408555ee92566c94a6f692
brandon <brandonochoa202030023@cunoc.edu.gt>
2026-09-05 02:38:10 -0600

    Primer commit y creando proyecto

---
+++ Proyecto1OLC2/src/main/java/com/mycompany/proyecto1olc2/Proyecto1OLC2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto1olc2;
+
+/**
+ *
+ * @author brandon
+ */
+public class Proyecto1OLC2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
